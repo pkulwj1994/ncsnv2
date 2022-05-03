ec56bf8e580a5dae786c4404d11097c2fb2f360e
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-05-03 13:44:52 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -54,6 +54,45 @@ def anneal_Langevin_dynamics(x_mod, scorenet, sigmas, n_steps_each=200, step_lr=
             return [x_mod.to('cpu')]
         else:
             return images
+        
+@torch.no_grad()
+def anneal_Langevin_caliberation(x_mod, basescorenet, scorenet, sigmas, lam=1.0, n_steps_each=200, step_lr=0.000008,
+                             final_only=False, verbose=False, denoise=True):
+    images = []
+
+    with torch.no_grad():
+        for c, sigma in enumerate(sigmas):
+            labels = torch.ones(x_mod.shape[0], device=x_mod.device) * c
+            labels = labels.long()
+            step_size = step_lr * (sigma / sigmas[-1]) ** 2
+            for s in range(n_steps_each):
+                grad = basescorenet(x_mod,labels) - scorenet(x_mod, labels)/lam
+
+                noise = torch.randn_like(x_mod)
+                grad_norm = torch.norm(grad.view(grad.shape[0], -1), dim=-1).mean()
+                noise_norm = torch.norm(noise.view(noise.shape[0], -1), dim=-1).mean()
+                x_mod = x_mod + step_size * grad + noise * np.sqrt(step_size * 2)
+
+                image_norm = torch.norm(x_mod.view(x_mod.shape[0], -1), dim=-1).mean()
+                snr = np.sqrt(step_size / 2.) * grad_norm / noise_norm
+                grad_mean_norm = torch.norm(grad.mean(dim=0).view(-1)) ** 2 * sigma ** 2
+
+                if not final_only:
+                    images.append(x_mod.to('cpu'))
+                if verbose:
+                    print("level: {}, step_size: {}, grad_norm: {}, image_norm: {}, snr: {}, grad_mean_norm: {}".format(
+                        c, step_size, grad_norm.item(), image_norm.item(), snr.item(), grad_mean_norm.item()))
+
+        if denoise:
+            last_noise = (len(sigmas) - 1) * torch.ones(x_mod.shape[0], device=x_mod.device)
+            last_noise = last_noise.long()
+            x_mod = x_mod + sigmas[-1] ** 2 * (basescorenet(x_mod, last_noise) - scorenet(x_mod, last_noise)/lam)
+            images.append(x_mod.to('cpu'))
+
+        if final_only:
+            return [x_mod.to('cpu')]
+        else:
+            return images
 
 
 @torch.no_grad()
