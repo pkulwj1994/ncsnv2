90154d8b6c8c00c3a68f6e009c46aec0a03a69d5
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-05-04 10:22:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,57 +8,73 @@ import sys
 import os
 import torch
 import numpy as np
+import torch.utils.tensorboard as tb
+import copy
 from runners import *
 
+import os
 
 def parse_args_and_config():
     parser = argparse.ArgumentParser(description=globals()['__doc__'])
 
-    parser.add_argument('--runner', type=str, default='AnnealRunner', help='The runner to execute')
-    parser.add_argument('--config', type=str, default='anneal.yml',  help='Path to the config file')
+    parser.add_argument('--config', type=str, required=True,  help='Path to the config file')
     parser.add_argument('--seed', type=int, default=1234, help='Random seed')
-    parser.add_argument('--run', type=str, default='run', help='Path for saving running related data.')
-    parser.add_argument('--doc', type=str, default='0', help='A string for documentation purpose')
+    parser.add_argument('--exp', type=str, default='exp', help='Path for saving running related data.')
+    parser.add_argument('--doc', type=str, required=True, help='A string for documentation purpose. '
+                                                               'Will be the name of the log folder.')
     parser.add_argument('--comment', type=str, default='', help='A string for experiment comment')
     parser.add_argument('--verbose', type=str, default='info', help='Verbose level: info | debug | warning | critical')
     parser.add_argument('--test', action='store_true', help='Whether to test the model')
-    parser.add_argument('--sample', action='store_true', help='Whether to test the model')
-    parser.add_argument('--fast_fid', action='store_true', help='Whether to test the model')
+    parser.add_argument('--sample', action='store_true', help='Whether to produce samples from the model')
+    parser.add_argument('--fast_fid', action='store_true', help='Whether to do fast fid test')
     parser.add_argument('--resume_training', action='store_true', help='Whether to resume training')
-    parser.add_argument('-o', '--image_folder', type=str, default='images', help="The directory of image outputs")
+    parser.add_argument('-i', '--image_folder', type=str, default='images', help="The folder name of samples")
+    parser.add_argument('--ni', action='store_true', help="No interaction. Suitable for Slurm Job launcher")
 
     args = parser.parse_args()
-    run_id = str(os.getpid())
-    run_time = time.strftime('%Y-%b-%d-%H-%M-%S')
-    # args.doc = '_'.join([args.doc, run_id, run_time])
-    args.log = os.path.join(args.run, 'logs', args.doc)
+    args.log_path = os.path.join(args.exp, 'logs', args.doc)
 
     # parse config file
-    if not args.test and not args.sample and not args.fast_fid:
-        with open(os.path.join('configs', args.config), 'r') as f:
-            config = yaml.load(f)
-        new_config = dict2namespace(config)
-    else:
-        with open(os.path.join(args.log, 'config.yml'), 'r') as f:
-            config = yaml.load(f)
-        new_config = config
+    with open(os.path.join('configs', args.config), 'r') as f:
+        config = yaml.load(f)
+    new_config = dict2namespace(config)
+
+    tb_path = os.path.join(args.exp, 'tensorboard', args.doc)
 
     if not args.test and not args.sample and not args.fast_fid:
         if not args.resume_training:
-            if os.path.exists(args.log):
-                shutil.rmtree(args.log)
-            os.makedirs(args.log)
+            if os.path.exists(args.log_path):
+                overwrite = False
+                if args.ni:
+                    overwrite = True
+                else:
+                    response = input("Folder already exists. Overwrite? (Y/N)")
+                    if response.upper() == 'Y':
+                        overwrite = True
 
-        with open(os.path.join(args.log, 'config.yml'), 'w') as f:
-            yaml.dump(new_config, f, default_flow_style=False)
+                if overwrite:
+                    shutil.rmtree(args.log_path)
+                    shutil.rmtree(tb_path)
+                    os.makedirs(args.log_path)
+                    if os.path.exists(tb_path):
+                        shutil.rmtree(tb_path)
+                else:
+                    print("Folder exists. Program halted.")
+                    sys.exit(0)
+            else:
+                os.makedirs(args.log_path)
 
+            with open(os.path.join(args.log_path, 'config.yml'), 'w') as f:
+                yaml.dump(new_config, f, default_flow_style=False)
+
+        new_config.tb_logger = tb.SummaryWriter(log_dir=tb_path)
         # setup logger
         level = getattr(logging, args.verbose.upper(), None)
         if not isinstance(level, int):
             raise ValueError('level {} not supported'.format(args.verbose))
 
         handler1 = logging.StreamHandler()
-        handler2 = logging.FileHandler(os.path.join(args.log, 'stdout.txt'))
+        handler2 = logging.FileHandler(os.path.join(args.log_path, 'stdout.txt'))
         formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
         handler1.setFormatter(formatter)
         handler2.setFormatter(formatter)
@@ -80,18 +96,18 @@ def parse_args_and_config():
         logger.setLevel(level)
 
         if args.sample:
-            os.makedirs(os.path.join(args.run, 'image_samples'), exist_ok=True)
-            args.image_folder = os.path.join(args.run, 'image_samples', args.image_folder)
+            os.makedirs(os.path.join(args.exp, 'image_samples'), exist_ok=True)
+            args.image_folder = os.path.join(args.exp, 'image_samples', args.image_folder)
             if not os.path.exists(args.image_folder):
                 os.makedirs(args.image_folder)
             else:
-                overwrite = True
-                # if args.ni:
-                #     overwrite = True
-                # else:
-                #     response = input("Image folder already exists. Overwrite? (Y/N)")
-                #     if response.upper() == 'Y':
-                #         overwrite = True
+                overwrite = False
+                if args.ni:
+                    overwrite = True
+                else:
+                    response = input("Image folder already exists. Overwrite? (Y/N)")
+                    if response.upper() == 'Y':
+                        overwrite = True
 
                 if overwrite:
                     shutil.rmtree(args.image_folder)
@@ -101,20 +117,20 @@ def parse_args_and_config():
                     sys.exit(0)
 
         elif args.fast_fid:
-            os.makedirs(os.path.join(args.run, 'fid_samples'), exist_ok=True)
-            args.image_folder = os.path.join(args.run, 'fid_samples', args.image_folder)
+            os.makedirs(os.path.join(args.exp, 'fid_samples'), exist_ok=True)
+            args.image_folder = os.path.join(args.exp, 'fid_samples', args.image_folder)
             if not os.path.exists(args.image_folder):
                 os.makedirs(args.image_folder)
             else:
-                overwrite = True
-                # if args.ni:
-                #     overwrite = False
-                # else:
-                #     response = input("Image folder already exists. \n "
-                #                      "Type Y to delete and start from an empty folder?\n"
-                #                      "Type N to overwrite existing folders (Y/N)")
-                #     if response.upper() == 'Y':
-                #         overwrite = True
+                overwrite = False
+                if args.ni:
+                    overwrite = False
+                else:
+                    response = input("Image folder already exists. \n "
+                                     "Type Y to delete and start from an empty folder?\n"
+                                     "Type N to overwrite existing folders (Y/N)")
+                    if response.upper() == 'Y':
+                        overwrite = True
 
                 if overwrite:
                     shutil.rmtree(args.image_folder)
@@ -149,17 +165,19 @@ def dict2namespace(config):
 
 def main():
     args, config = parse_args_and_config()
-    logging.info("Writing log file to {}".format(args.log))
+    logging.info("Writing log file to {}".format(args.log_path))
     logging.info("Exp instance id = {}".format(os.getpid()))
     logging.info("Exp comment = {}".format(args.comment))
     logging.info("Config =")
     print(">" * 80)
-    print(config)
+    config_dict = copy.copy(vars(config))
+    if not args.test and not args.sample and not args.fast_fid:
+        del config_dict['tb_logger']
+    print(yaml.dump(config_dict, default_flow_style=False))
     print("<" * 80)
 
     try:
-        runner = eval(args.runner)(args, config)
-
+        runner = NCSNRunner(args, config)
         if args.test:
             runner.test()
         elif args.sample:
@@ -168,7 +186,6 @@ def main():
             runner.fast_fid()
         else:
             runner.train()
-
     except:
         logging.error(traceback.format_exc())
 
